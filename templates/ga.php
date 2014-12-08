a25ea6031cb2396534a3ab272cf31d9b52dfdad8
xiaoqing.dongxq <xiaoqing.dongxq@alibaba-inc.com>
2014-12-08 18:10:29 +0800

    change ga

--- templates/ga.php
+++ templates/ga.php
@@ -1,1 +1,1 @@
-<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1253711425'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s95.cnzz.com/z_stat.php%3Fid%3D1253711425' type='text/javascript'%3E%3C/script%3E"));</script>
+<script src="http://w.cnzz.com/q_stat.php?id=1253711425"></script> 
